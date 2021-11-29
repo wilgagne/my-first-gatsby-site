fd42b147e7d1ca930494a31a9fb90a8a8143831d
wilgagne <william.gagne@mail.utoronto.ca>
2021-11-29 01:04:22 -0500

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => {
   return (
       <main>
         <title>Home Page</title>
-        <h1>Welcome to my Gatsby site!</h1>
+        <h1>Hello Amigo</h1>
           <Link to="/about">About</Link>
         <p>I'm making this by following the Gatsby Tutorial.</p>
       </main>
